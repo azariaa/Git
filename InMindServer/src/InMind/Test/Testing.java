1390aa82bbea78d9e177195f41fd717b2b8af597
azariaa <azariaa@cs.cmu.edu>
2015-12-02 18:35:02 -0500

    updates

--- InMindServer/src/InMind/Test/Testing.java
+++ InMindServer/src/InMind/Test/Testing.java
@@ -15,9 +15,13 @@ public class Testing
         try
         {
             Map<String, String> retParameters = new HashMap<>();
-            retParameters.put("userId", "my userId");
+            //retParameters.put("userId", "my userId");
             //dialogUtils.callServer("https://talkingtothecrowd.org:" + 1606 + "/Ugxe8mCBxzxh21V5Vuh4AaABAQ", retParameters, true);
-            String response = dialogUtils.callServer("http://45.55.172.104:" + 1606 + "/", retParameters, true);
+            //String response = dialogUtils.callServer("http://45.55.172.104:" + 1606 + "/", retParameters, true);
+            retParameters.put("Body", "Do you want to meet at 2:00 to discuss about the birthday party for Bob?");
+            retParameters.put("Sender", "Alan Black");
+            retParameters.put("Subject", "Birthday party");
+            String response = dialogUtils.callServer("http://birch.speech.cs.cmu.edu:5000/distract", retParameters, true);
             System.out.println(response);
         } catch (Exception ex)
         {
